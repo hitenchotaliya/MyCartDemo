a5d431715a92c1a5ae71bf8c2b2b603027110f8b
hiten <chotaliyahiten672@gmail.com>
2024-02-23 14:54:54 +0530

    Update Search css

--- categories.php
+++ categories.php
@@ -112,6 +112,7 @@
     <head>
         <link rel="stylesheet" href="./css/main.css">
         <link rel="stylesheet" href="./css/table-sort.css">
+
         <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" /> -->
     </head>
 
